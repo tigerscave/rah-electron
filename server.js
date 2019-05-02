b3e19062560cb93c1f29173b91409bc6e4183475
rhmt <rezaeihazara@gmail.com>
2019-05-02 11:29:57 +0900

    feat: add receiver peer connection

--- server.js
+++ server.js
@@ -10,30 +10,50 @@ let connections = [];
 let users = []
 
 server.listen(port, () => {
-    console.log('Server running ...', port);
+  console.log('Server running ...', port);
 })
 
 app.use(express.static('src'))
 
+const handleOfferFromSender = (socket, data) => {
+  const { description, userId } = data;
+  socket.to(userId).emit('offerToReceiver', {
+    description,
+    senderId: socket.id
+  })
+};
+
+const handleAnswerFromReceiver = (socket, data) => {
+  const { userId, description } = data;
+  socket.to(userId).emit('answerToSender', description)
+}
+
 io.on('connection', (socket) => {
-    connections.push(socket);
-    users.push(socket);
-    console.log('connected: %s sockets connected', connections.length, users)
-    
-    // Disconnect
-    socket.on('disconnect', () => {
-        connections.splice(connections.indexOf(socket), 1);
-        users.filter(user => user !== socket.id);
-        console.log('Disconnected: %s sockets connected', connections.length);
-    });
-
-    // Send Messages
-    socket.emit('send messages', (msg) => {
-        console.log('messssssss', msg)
-    })
-
-    socket.on('sendmessages', (msg) => {
-        console.log('messages', msg);
-        socket.emit('send messages', msg)
-    })
+  connections.push(socket);
+  users.push(socket);
+  console.log('connected: %s sockets connected', connections.length, users)
+  
+  // Disconnect
+  socket.on('disconnect', () => {
+    connections.splice(connections.indexOf(socket), 1);
+    users.filter(user => user !== socket.id);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+  
+  // Send Messages
+  socket.emit('send messages', (msg) => {
+    console.log('messssssss', msg)
+  })
+  
+  socket.on('sendmessages', (msg) => {
+    console.log('messages', msg);
+    socket.emit('send messages', msg)
+  })
+  
+  socket.on('offerFromSender', data => {
+    handleOfferFromSender(socket, data)
+  })
+  socket.on('answerFromReceiver', data => {
+    handleAnswerFromReceiver(socket, data)
+  })
 })
